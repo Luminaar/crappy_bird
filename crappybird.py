887917c6568704c93fd94d00451271873e27d528
Luminaar <kovykmax@gmail.com>
2017-08-21 12:49:30 +0200

    Add second wall

--- crappybird.py
+++ crappybird.py
@@ -24,6 +24,103 @@ class Const():
     INFO_LABEL = FONT.render('R - reset  Q - quit', 1, BLUE)
 
 
+class Wall():
+
+    def __init__(self, name, *actors):
+        self.name = name
+        self.actors = []
+        self.animations = []
+
+        self.trigger = Rect((actors[0].x, 0),
+                            (Const.WALL_W, Const.HEIGHT))
+        self.triggered = False
+
+        for actor in actors:
+            self.actors.append(actor)
+
+
+    def draw(self):
+        for actor in self.actors:
+            actor.draw()
+
+
+    def animate(self, tween='linear', duration=1, on_finished=None, **targets):
+        """Set animations for all actors in the group. All actors will
+        have the same animation."""
+
+        for actor in self.actors:
+            anim = Animation(actor,
+                             tween,
+                             duration,
+                             **targets)
+
+            self.animations.append(anim)
+
+        trigger_anim = Animation(self.trigger,
+                                 tween,
+                                 duration,
+                                 on_finished=on_finished,
+                                 **targets)
+
+        self.animations.append(trigger_anim)
+
+
+    def stop_animations(self):
+        """Stop all animations."""
+
+        for anim in self.animations:
+            try:
+                anim.stop()
+            except ValueError:
+                pass
+
+        self.animations = []
+
+
+    def collides(self, actor):
+        """Return True if 'actor' collides with any actors in this
+        group."""
+
+        for a in self.actors:
+            if a.colliderect(actor):
+                return True
+        return False
+
+
+    def collides_trigger(self, actor):
+        """Check collision with the trigger. Don't allow multiple
+        collisions."""
+
+        if self.trigger.colliderect(actor):
+            self.triggered = True
+            return True
+
+        return False
+
+
+    def reset_trigger(self):
+        self.triggered = False
+
+
+    def reset(self, x=Const.WIDTH):
+        """Reset wall trigger and put it in the initial position at 'x'
+        coordinate."""
+
+        offset = randint(100, 300)
+        gap = randint(210, 250)
+
+        top_y = 0 - Const.WALL_H + offset
+        bottom_y = top_y + Const.WALL_H + gap
+
+        self.actors[0].topleft = (x, top_y)
+        self.actors[1].topleft = (x, bottom_y)
+        self.trigger.x = x
+
+        self.reset_trigger()
+        self.animate(duration=Const.WALL_DURATION,
+                     x=0 - Const.WALL_W,
+                     on_finished=self.reset)
+
 
 class Game():
 
@@ -40,14 +137,30 @@ class Game():
 
         cls.alien = alien
 
-        walls = [Actor('top',
-                       (Const.WIDTH - Const.WALL_W,
-                        0 - Const.WALL_H + 100)),
-                 Actor('bottom',
-                       (Const.WIDTH - Const.WALL_W,
-                        Const.HEIGHT - 250))]
+        first_wall = Wall('first_wall',
+                          Actor('top',
+                                topleft=(Const.WIDTH,
+                                         0 - Const.WALL_H + 100)),
+                          Actor('bottom',
+                                topleft=(Const.WIDTH,
+                                         Const.HEIGHT - 250)))
+        first_wall.animate(duration=Const.WALL_DURATION,
+                           on_finished=first_wall.reset,
+                           x=(0-Const.WALL_W))
 
-        cls.walls = walls
+
+        second_wall = Wall('second_wall',
+                           Actor('top',
+                                 topleft=(Const.WIDTH + Const.WIDTH/2,
+                                          0 - Const.WALL_H + 100)),
+                           Actor('bottom',
+                                 topleft=(Const.WIDTH + Const.WIDTH/2,
+                                          Const.HEIGHT - 250)))
+        second_wall.animate(duration=Const.WALL_DURATION * 1.5,
+                            on_finished=second_wall.reset,
+                            x=(0-Const.WALL_W))
+
+        cls.walls = [first_wall, second_wall]
 
         cls.trigger = Rect((Const.WIDTH, 0),
                            (Const.WALL_W, Const.HEIGHT))
@@ -55,54 +168,6 @@ class Game():
         ground = Rect((0, Const.HEIGHT),
                       (Const.WIDTH, 100))
         cls.ground = ground
-
-        cls.reset()
-
-
-    @classmethod
-    def reset(cls):
-        walls = cls.walls
-        trigger = cls.trigger
-
-        offset = randint(100, 300)
-        gap = randint(210, 250)
-
-        top_x = walls[0].topleft[0]
-        bottom_x = walls[1].topleft[0]
-
-        top_y = 0 - Const.WALL_H + offset
-        bottom_y = top_y + Const.WALL_H + gap
-
-        walls[0].topleft = (top_x, top_y)
-        walls[1].topleft = (bottom_x, bottom_y)
-
-        cls.wall_animations = []
-        cls.trigger_anim = None
-
-        trigger.x = Const.WIDTH
-        walls[0].x = Const.WIDTH
-        walls[1].x = Const.WIDTH
-
-        cls.triggered = False
-        trigger_anim = Animation(trigger,
-                                 duration=Const.WALL_DURATION,
-                                 x=0 - Const.WALL_W,
-                                 y=trigger.y)
-
-        top_wall_anim = Animation(walls[0],
-                                  duration=Const.WALL_DURATION,
-                                  x=0 - Const.WALL_W,
-                                  y=walls[0].y)
-
-        bottom_wall_anim = Animation(walls[1],
-                                     duration=Const.WALL_DURATION,
-                                     x=0 - Const.WALL_W,
-                                     y=walls[1].y,
-                                     on_finished=Game.reset)
-
-        cls.wall_animations.append(top_wall_anim)
-        cls.wall_animations.append(bottom_wall_anim)
-        cls.trigger_anim = trigger_anim
 
 
 class Alien(Actor):
@@ -124,10 +189,9 @@ class Alien(Actor):
         self.alive = False
         self.image = 'alien_hurt'
 
-        for anim in Game.wall_animations:
-            anim.stop()
+        for wall in Game.walls:
+            wall.stop_animations()
 
-        Game.trigger_anim.stop()
 
         death_anim = animate(self,
                              pos=(self.x, self.y-100),
@@ -210,8 +274,11 @@ def update():
         if collides(alien, Game.ground):
             alien.kill()
 
-        if collides(alien, walls):
-            alien.kill()
+        for wall in walls:
+            if wall.collides(alien):
+                alien.kill()
+            if not wall.triggered and wall.collides_trigger(alien):
+                Game.counter += 1
 
 
 Game.start()
